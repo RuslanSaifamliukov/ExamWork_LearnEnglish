fb3a3cf58816901274935f6b108499769e797f77
RuslanSaifamliukov <alik8066@gmail.com>
2023-05-26 13:58:48 +0500

    ExamineWort

--- app/src/main/java/com/example/saifapp5/RegistrActivity.kt
+++ app/src/main/java/com/example/saifapp5/RegistrActivity.kt
@@ -10,17 +10,15 @@ import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import java.util.regex.Pattern
 
-class RegistrActivity : AppCompatActivity()
-{
-    var preff: SharedPreferences? = null
-    val pattern2 = ("[a-zA-Z]{1,34}")
-    val pattern = ("[a-z0-9A-Z]{1,34}" + "@" + "[a-z]{1,6}" + "\\." + "[a-z]{1,5}")
-    val pattern3 = ("[a-z0-9A-Z]{1,34}")
+class RegistrActivity : AppCompatActivity() {
 
     lateinit var name: TextInputEditText
     lateinit var mail: TextInputEditText
     lateinit var pass: TextInputEditText
     lateinit var repass: TextInputEditText
+    lateinit var lname: TextInputEditText
+
+    private val userService: UserService = UserService()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registr)
@@ -28,76 +26,41 @@ class RegistrActivity : AppCompatActivity()
         mail = findViewById(R.id.etMail)
         pass = findViewById(R.id.etPassword)
         repass = findViewById(R.id.etConfirmPassword)
-        preff = getSharedPreferences("TABLEE", MODE_PRIVATE)
-        name.setText(preff?.getString("key1", ""))
-        mail.setText(preff?.getString("key2", ""))
-        pass.setText(preff?.getString("key3", ""))
-    }
+        lname = findViewById(R.id.etName)
 
-    fun emailVaild(text: String): Boolean {
-        return Pattern.compile(pattern).matcher(text).matches()
+        userService.Init(this)
     }
-    fun nameVaild(text: String): Boolean {
-        return Pattern.compile(pattern2).matcher(text).matches()
-    }
-    fun passVaild(text: String): Boolean {
-        return Pattern.compile(pattern3).matcher(text).matches()
-    }
-    fun saveData(name: String, mail: String, pass: String) {
-        val editor = preff?.edit()
-        editor?.putString("key1", name)
-        editor?.putString("key2", mail)
-        editor?.putString("key3", pass)
-        editor?.apply()
-    }
+    fun ButRegistr(view: View) {
+        val nameString = name.text.toString().trim().lowercase()
+        val mailString = mail.text.toString().trim()
+        val passString = pass.text.toString().trim()
+        val repassString = repass.text.toString().trim()
+        val lnameString = name.text.toString().trim()
 
-    fun ButRegistr(view: View)
-    {
-        if (name.text.toString().isNotEmpty() &&
-            mail.text.toString().isNotEmpty() &&
-            pass.text.toString().isNotEmpty() &&
-            repass.text.toString().isNotEmpty())
-        {
-            if (nameVaild(name.text.toString()))
-            {
-                if (emailVaild(mail.text.toString()))
-                {
-                    if (passVaild(pass.text.toString())&&passVaild(repass.text.toString()))
-                    {
-                        val value: String = name.text.toString()
-                        val value2: String = mail.text.toString()
-                        val value3: String = pass.text.toString()
-                        if(pass.text.toString()==repass.text.toString())
-                        {
-                            saveData(value, value2, value3)
-                            val intent = Intent(this@RegistrActivity, SignInActivity::class.java)
-                            startActivity(intent)
-                            Toast.makeText(this, "Регистрация прошла успешно!", Toast.LENGTH_SHORT).show()
-                            finish()
-                        }
-                        else
-                        {
-                            Toast.makeText(this, "Пароли не совпадают!", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                    else
-                    {
-                        Toast.makeText(this, "Пароль содержит не допустимые символы!", Toast.LENGTH_SHORT).show()
-                    }
-                }
-                else
-                {
-                    Toast.makeText(this, "E-mail введён не корректно!", Toast.LENGTH_SHORT).show()
-                }
-            }
-            else
-            {
-                Toast.makeText(this, "Имя или Фамилия содержит не допустимые символы!", Toast.LENGTH_SHORT).show()
-            }
-        }
-        else
-        {
+        if (nameString.isNullOrBlank())
             Toast.makeText(this, "Заполните пустые поля!", Toast.LENGTH_SHORT).show()
+
+        var userCheckStatus = userService.credentialsStatus(mailString, passString, repassString)
+        when (userCheckStatus) {
+            UserCheckStatus.Empty
+            -> Toast.makeText(this, "Заполните пустые поля!", Toast.LENGTH_SHORT).show()
+
+            UserCheckStatus.WrongEmail
+            -> Toast.makeText(this, "Емайл введен некорректно!", Toast.LENGTH_SHORT).show()
+
+            UserCheckStatus.PasswordsNotEqual
+            -> Toast.makeText(this, "Пароли не совпадают!", Toast.LENGTH_SHORT).show()
+
+            UserCheckStatus.WrongEmailOrPassword
+            -> throw IllegalArgumentException("При регистрации не может быть неверный пароль")
+
+            UserCheckStatus.OK -> {
+                userService.saveUser(mailString, passString, nameString, lnameString)
+                Toast.makeText(this, "Регистрация прошла успешно!", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@RegistrActivity, SignInActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
     }
 
--- app/src/main/java/com/example/saifapp5/SignInActivity.kt
+++ app/src/main/java/com/example/saifapp5/SignInActivity.kt
@@ -1,103 +1,68 @@
 package com.example.saifapp5
 
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.CheckBox
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
-import java.util.regex.Pattern
-
 
 class SignInActivity : AppCompatActivity() {
 
-
-
-    var preff: SharedPreferences? = null
-    val pattern = ("[a-z0-9A-Z]{1,34}" + "@" + "[a-z]{1,6}" + "\\." + "[a-z]{1,5}")
-    val pattern2 = ("[a-z0-9A-Z]{1,34}")
-
     lateinit var mail: TextInputEditText
     lateinit var pass: TextInputEditText
-    lateinit var check: CheckBox
+    lateinit var remember: CheckBox
+    val userService: UserService = UserService()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
         mail = findViewById(R.id.Mail)
         pass = findViewById(R.id.Password)
-        check = findViewById(R.id.checkBox)
-        preff = getSharedPreferences("TABLEE", MODE_PRIVATE)
-        check.isChecked = preff?.getBoolean("key4", false) ?: false
-        mail.setText(preff?.getString("key2", ""))
-        pass.setText(preff?.getString("key3", ""))
-    }
+        remember = findViewById(R.id.checkBox)
 
-    fun savestate(check: Boolean) {
-        val editor = preff?.edit()
-        editor?.putBoolean("key4", check)
-        editor?.apply()
-    }
-
-    fun saveData(mail: String, pass: String) {
-        val editor = preff?.edit()
-        editor?.putString("key2", mail)
-        editor?.putString("key3", pass)
-        editor?.apply()
-    }
-
-    fun emailVaild(text: String): Boolean {
-        return Pattern.compile(pattern).matcher(text).matches()
-    }
-
-    fun passVaild(text: String): Boolean {
-        return Pattern.compile(pattern2).matcher(text).matches()
-    }
-
-    fun GoListen(view: View)
-    {
-
-        if (mail.text.toString().isNotEmpty() && pass.text.toString().isNotEmpty())
-        {
-            if (emailVaild(mail.text.toString()))
-            {
-                if (passVaild(pass.text.toString()))
-                {
-                    val value: String = mail.text.toString()
-                    val value2: String = pass.text.toString()
-                    val checkboxstate: Boolean = check.isChecked
-                    if (checkboxstate == true)
-                    {
-                        saveData(value, value2)
-                        savestate(checkboxstate)
-                        val intent = Intent(this@SignInActivity, ListenActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    }
-                    else
-                    {
-                        val intent = Intent(this@SignInActivity, ListenActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    }
-                }
-                else
-                {
-                    Toast.makeText(this, "Пароль содержит не допустимые символы!", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            else
-            {
-                Toast.makeText(this, "E-mail введён не корректно!", Toast.LENGTH_SHORT).show()
-            }
-
+        userService.Init(this)
+        remember.isChecked = userService.rememberCredentials
+        if (remember.isChecked) {
+            mail.setText(userService.signinEmail)
+            pass.setText(userService.signinPass)
         }
-        else
-        {
-            Toast.makeText(this, "Введите данные для входа!", Toast.LENGTH_SHORT).show()
+    }
+    fun GoListen(view: View) {
+        userService.rememberCredentials = remember.isChecked
+        if (!remember.isChecked)
+            userService.cleanLoginScreenData()
+
+        val mailString = mail.text.toString().trim().lowercase()
+        val passString = pass.text.toString().trim()
+
+        var userCheckStatus = userService.credentialsStatus(mailString, passString)
+        when (userCheckStatus) {
+            UserCheckStatus.Empty
+            -> Toast.makeText(this, "Введите Емайл и Пароль!", Toast.LENGTH_SHORT).show()
+
+            UserCheckStatus.WrongEmail
+            -> Toast.makeText(this, "E-mail введён не корректно!", Toast.LENGTH_SHORT).show()
+
+            UserCheckStatus.WrongEmailOrPassword
+            -> Toast.makeText(this, "Неверный Емайл или Пароль", Toast.LENGTH_SHORT).show()
+
+            UserCheckStatus.PasswordsNotEqual
+            -> throw IllegalArgumentException("При входе не может быть разных паролей")
+
+            UserCheckStatus.OK -> {
+                if (remember.isChecked)
+                    userService.saveLoginScreenData(mailString, passString)
+
+                goToListen()
+            }
         }
+    }
+
+    fun goToListen() {
+        val intent = Intent(this@SignInActivity, ListenActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     fun ForwardRegistration(view: View) {
